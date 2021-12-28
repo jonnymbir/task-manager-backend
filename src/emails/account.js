a19270f341c4360678dd68a72f2fac6df5c354f4
John Kofi Mbir <jkmbir@gmail.com>
2021-12-28 15:06:59 +0000

    finalised email notifications

--- src/emails/account.js
+++ src/emails/account.js
@@ -7,14 +7,24 @@ sgMail.setApiKey(sendgridAPIKey);
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'jkmbir@gmail.com',
+        from: 'jmbir@rxhealthinfosystems.com',
         subject: 'Welcome to the App',
         text: `Welcome to the app, ${name} I hope this one actually gets to you now!!!.`,
     }).catch((e)=> console.log(e))
 }
 
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'jmbir@rxhealthinfosystems.com',
+        subject: 'Sorry to see you go',
+        text: `Sorry, ${name} is there anything we can do to improve our service!!!.`,
+    }).catch((e)=> console.log(e))
+}
+
 module.exports = {
-    sendWelcomeEmail
+    sendWelcomeEmail,
+    sendCancellationEmail
 };
 
     // sgMail.send({
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,12 +3,14 @@ const User = require('../models/user');
 const sharp = require('sharp');
 const auth = require('../middleware/auth');
 const multer = require('multer');
+const { sendWelcomeEmail, sendCancellationEmail } = require('../emails/account');
 const router = new express.Router();
 
 router.post('/users', async (req, res)=>{
     const user = new User(req.body);
     try {
         await user.save();
+        sendWelcomeEmail(user.email, user.name);
         const token = await user.generateAuthToken();
         res.status(201).send({user:user, token:token}); 
     } catch (error) {
@@ -92,6 +94,7 @@ router.patch('/users/me', auth, async (req, res)=>{
 router.delete('/users/me', auth, async(req, res)=>{
     try {
         await req.user.remove();
+        sendCancellationEmail(req.user.email, req.user.name);
         res.status(200).send(req.user);
     } catch (e) {
         res.status(400).send(e);
